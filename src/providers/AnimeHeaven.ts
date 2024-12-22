cb24f2bcf6e1e5b074155b4e46d241b1fa2b6161
Alessio Ganzarolli <ganzarolli.alessio@gmail.com>
2024-12-22 02:59:06 +0100

    feat: AnimeHeaven

---
+++ src/providers/AnimeHeaven.ts
@@ -0,0 +1,120 @@
+import axios from "axios";
+import { load } from "cheerio";
+import Provider from "../models/provider";
+import { Search, MediaResult, MediaInfo, Sources } from "../models/types";
+
+class AnimeHeaven extends Provider {
+  override readonly name = "AnimeHeaven";
+  protected override baseUrl = "https://animeheaven.me";
+  protected languages = "en";
+  protected colorHEX = "#e81e2f";
+  protected override logo = "https://animeheaven.me/favicon.ico";
+
+  override async search(query: string): Promise<Search<MediaResult>> {
+    try {
+      const res = await axios.get(`${this.baseUrl}/search.php?s=${query}`);
+      const $ = load(res.data);
+
+      if (!$) return { results: [] };
+
+      const searchResult: {
+        hasNextPage: boolean;
+        results: MediaResult[];
+      } = {
+        hasNextPage: false,
+        results: [],
+      };
+
+      $(".similarimg").each((_, el) => {
+        searchResult.results.push({
+          id: $(el)?.find("a")?.attr("href")?.split("anime.php?")[1] ?? "",
+          title: $(el)?.find(".similarname.c a.c")?.text().trim(),
+          image: `${this.baseUrl}/${$(el)?.find("img.coverimg")?.attr("src")}`,
+        });
+      });
+
+      return searchResult;
+    } catch (err) {
+      throw new Error((err as Error).message);
+    }
+  }
+
+  override async fetchInfo(id: string): Promise<MediaInfo> {
+    try {
+      // don't ask
+      const res = await fetch(`${this.baseUrl}/anime.php?${id}`, {
+        method: "GET",
+      });
+      const $ = load(await res.text());
+
+      // return {} as MediaInfo
+
+      const info: MediaInfo = {
+        id,
+        hasSeasons: false,
+        title: $(".infotitle.c")?.text().trim(),
+        description: $(".infodes.c")?.text().trim(),
+        genres: $("boxitem.bc2.c1")
+          .map((_, el) => $(el).text().trim())
+          .get(),
+        totalEpisodes: parseInt($($(".inline.c2")[0]).text().trim()),
+        releaseDate: {
+          year: parseInt($($(".inline.c2")[1]).text().split("/")[0].trim()),
+        },
+        rating: parseFloat($($(".inline.c2")[2]).text().trim().split("/")[0]),
+        cover: $(".posterimg").attr("src"),
+        synonyms: $(".infotitlejp.c")
+          ? [$(".infotitlejp.c").text().trim()]
+          : undefined,
+        episodes: [],
+      };
+
+      $(".ac3").each((_, el) => {
+        info.episodes?.push({
+          id: $(el).attr("href")?.split("episode.php?")[1] ?? "",
+          number: parseFloat($(el).find(".watch2.bc").text().trim()),
+        });
+      });
+
+      return info;
+    } catch (err) {
+      console.log(err);
+      throw new Error((err as Error).message);
+    }
+  }
+
+  override async fetchSources(id: string): Promise<Sources> {
+    try {
+      const res = await fetch(`${this.baseUrl}/episode.php?${id}`, {
+        method: "GET",
+      });
+      const $ = load(await res.text());
+
+      const episodeSources: Sources = {
+        sources: [],
+      };
+
+      const source = $("#vid")?.find("source")?.eq(0)?.attr("src");
+      if (source)
+        episodeSources.sources.push({
+          url: source,
+          quality: "default",
+          isM3U8: false,
+        });
+
+      const altSource = $("#vid")?.find("source")?.eq(1)?.attr("src");
+      if (altSource)
+        episodeSources.sources.push({
+          url: altSource,
+          quality: "backup",
+          isM3U8: false,
+        });
+
+      return episodeSources;
+    } catch (err) {
+      throw new Error((err as Error).message);
+    }
+  }
+}
+
+export default AnimeHeaven;
