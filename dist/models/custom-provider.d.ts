ed7619d096ee7e9e70ebcd981e7680b104d462a2
aleganza <ganzarolli.alessio@gmail.com>
2025-02-13 19:57:26 +0100

    bho

--- dist/models/custom-provider.d.ts
+++
@@ -1,5 +0,0 @@
-import ProviderBuilder from "./provider-builder";
-declare abstract class CustomProvider extends ProviderBuilder {
-}
-export default CustomProvider;
-//# sourceMappingURL=custom-provider.d.ts.map
--- packages/api/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import "dotenv/config";
-import { VercelRequest, VercelResponse } from "@vercel/node";
-declare const _default: (req: VercelRequest, res: VercelResponse) => Promise<void>;
-export default _default;
-//# sourceMappingURL=index.d.ts.map
