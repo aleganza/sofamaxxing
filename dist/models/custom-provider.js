18899270b774bc2598bc0974bca8816abbf89a41
aleganza <ganzarolli.alessio@gmail.com>
2025-02-13 02:56:28 +0100

    build

--- dist/models/custom-provider.js
+++ dist/models/custom-provider.js
@@ -4,6 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const provider_builder_1 = __importDefault(require("./provider-builder"));
-class Provider extends provider_builder_1.default {
+class CustomProvider extends provider_builder_1.default {
 }
-exports.default = Provider;
+exports.default = CustomProvider;
